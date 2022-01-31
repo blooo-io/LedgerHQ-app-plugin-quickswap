93ea67106ac19795e8f734b58e4c180946fe05e5
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2022-01-31 08:54:20 +0000

    fix: test steps

--- tests/src/swap_exact_tokens_for_eth_supporting_fee_on_transfer_tokens.test.js
+++ tests/src/swap_exact_tokens_for_eth_supporting_fee_on_transfer_tokens.test.js
@@ -20,7 +20,7 @@ const devices = [
     {
         name: "nanos",
         label: "Nano S",
-        steps: 10, // <= Define the number of steps for this test case and this device
+        steps: 9, // <= Define the number of steps for this test case and this device
     },
     // {
     //   name: "nanox",
