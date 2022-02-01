c9765e482ef51f35260c1c19e4d584859a0f91d6
n4l5u0r <59141606+n4l5u0r@users.noreply.github.com>
2022-02-01 18:08:28 +0000

    fix: eth-sdk-plugin upgraded

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -1,16 +1,18 @@
 #include "quickswap_plugin.h"
 
 void handle_provide_token(void *parameters) {
-    ethPluginProvideToken_t *msg = (ethPluginProvideToken_t *) parameters;
+    ethPluginProvideInfo_t *msg = (ethPluginProvideInfo_t *) parameters;
     quickswap_parameters_t *context = (quickswap_parameters_t *) msg->pluginContext;
-    PRINTF("QUICKSWAP plugin provide token: 0x%p, 0x%p\n", msg->token1, msg->token2);
+    PRINTF("QUICKSWAP plugin provide token: 0x%p, 0x%p\n", msg->item1->token, msg->item2->token);
 
     if (ADDRESS_IS_NETWORK_TOKEN(context->contract_address_sent)) {
         context->decimals_sent = WEI_TO_ETHER;
         context->tokens_found |= TOKEN_SENT_FOUND;
-    } else if (msg->token1 != NULL) {
-        context->decimals_sent = msg->token1->decimals;
-        strlcpy(context->ticker_sent, (char *) msg->token1->ticker, sizeof(context->ticker_sent));
+    } else if (msg->item1 != NULL) {
+        context->decimals_sent = msg->item1->token.decimals;
+        strlcpy(context->ticker_sent,
+                (char *) msg->item1->token.ticker,
+                sizeof(context->ticker_sent));
         context->tokens_found |= TOKEN_SENT_FOUND;
     } else {
         // CAL did not find the token and token is not ETH.
@@ -22,10 +24,10 @@ void handle_provide_token(void *parameters) {
     if (ADDRESS_IS_NETWORK_TOKEN(context->contract_address_received)) {
         context->decimals_received = WEI_TO_ETHER;
         context->tokens_found |= TOKEN_RECEIVED_FOUND;
-    } else if (msg->token2 != NULL) {
-        context->decimals_received = msg->token2->decimals;
+    } else if (msg->item2 != NULL) {
+        context->decimals_received = msg->item2->token.decimals;
         strlcpy(context->ticker_received,
-                (char *) msg->token2->ticker,
+                (char *) msg->item2->token.ticker,
                 sizeof(context->ticker_received));
         context->tokens_found |= TOKEN_RECEIVED_FOUND;
     } else {
